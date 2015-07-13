f2df56abc5b9f76974dc46bd235720c9e45b4256
alan16 <alanmatias01@gmail.com>
2015-07-13 18:57:58 -0300

    Ejemplo clase 13-07

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Infraestructura.Contexto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Contexto())
+            {
+                var categorias = context.Clientes.ToList();                
+            }
+        }
+    }
+}
---
+++ Infraestructura/Contexto/Contexto.cs
@@ -0,0 +1,32 @@
+﻿using Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructura.Contexto
+{
+    public class Contexto:DbContext
+    {
+        public Contexto()
+            : base(@"Server=ALAN-PC\SQLEXPRESS;Database=CommerceProd;Integrated Security=SSPI;Trusted_Connection=false;Integrated Security=True") 
+        {
+            Configuration.LazyLoadingEnabled = false;
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Cliente> Clientes{ get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<DetalleCategoria> DetalleCategorias{ get; set; }
+        public DbSet<Factura> Facturas { get; set; }
+
+    }
+}
